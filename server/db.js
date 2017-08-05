1db03390c558176d076095278e330fc4a85c9bf6
simonol1 <simondanolearyis@gmail.com>
2017-08-05 23:41:43 +1000

    fixed tests, 4 passing now

--- server/db.js
+++ server/db.js
@@ -7,16 +7,7 @@ function getTestLib (connection) {
   return connection('testLib')
 }
 
-// function saveTestLib (connection) {
-//   test.id = tests.length + 1
-//   tests.push(tests)
-// }
 
 function getTechnologies (connection) {
   return connection('technologies')
 }
-
-// function saveTechnologies (test) {
-//   test.id = tests.length + 1
-//   tests.push(tests)
-// }
--- test/client/app.test.js
+++ test/client/app.test.js
@@ -10,11 +10,12 @@ App.prototype.componentDidMount = () => {}
 
 test('<App />', t => {
   const wrapper = shallow(<App />)
-  t.is(wrapper.find('h1').text(),'Widgets FTW!')
+  t.is(wrapper.find('h1').text(),'TEST IT!')
 })
 
 test.cb('mount <App />', t => {
   const wrapper = mount(<App />)
-  t.is(wrapper.find('.widget-list').exists(),true)
+  t.is(wrapper.find('.app-container').exists(),true)
+  t.is(wrapper.find('h4').text(), 'Test it real good.')
   t.end()
 })
